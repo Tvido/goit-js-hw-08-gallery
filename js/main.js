6dfb27a4c8929c23f3089e2ed2440bd78b718029
Volodymyr Viter <mad.tvido@gmail.com>
2021-03-05 15:02:45 +0200

    Upd

--- js/main.js
+++ js/main.js
@@ -1,26 +1,81 @@
 import gallery from './gallery-items.js';
 
-const galleryArr = document.querySelector('ul.js-gallery');
 const galleryItems = addGalleryItems(gallery);
+const galleryArr = document.querySelector('ul.js-gallery');
+const modalWindowRef = document.querySelector('div.js-lightbox');
+const modalImageRef = document.querySelector('img.lightbox__image');
+const closeModalBtnRef = document.querySelector('button[data-action]');
+const modaleOverlayRef = document.querySelector('div.lightbox__overlay');
 
 galleryArr.insertAdjacentHTML('beforeend', galleryItems);
 
+galleryArr.addEventListener('click', getOriginalImage);
+closeModalBtnRef.addEventListener('click', modalClose);
+modaleOverlayRef.addEventListener('click', modalClose);
+
+
 function addGalleryItems(gallery) {
-    // event.preventDefault();
-    return gallery.map(({ original, preview, description }) => {
-        return `<li class="gallery__item">
-  <a
-    class="gallery__link"
-    href="${original}"
-  >
-    <img
-      class="gallery__image"
+  return gallery.map(({ original, preview, description }) => {
+    return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img class="gallery__image"
       src="${preview}"
       data-source="${original}"
       alt="${description}"
     />
   </a>
 </li>`;
-    }).join('');
+  }).join('');
 };
 
+galleryArr.addEventListener('click', getOriginalImage);
+
+function getOriginalImage(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  };
+
+  const imageOrigin = event.target.dataset.source;
+  const imageAlt = event.target.alt;
+  const dataIndex = event.target.dataset.index;
+  modalOpen(imageOrigin, imageAlt, dataIndex);
+};
+
+// Закритя 1
+// function keyDownModuleWindow(event) {
+//   if (!modalWindowRef.classList.contains('is-open')) {
+//     return;
+//   };
+
+//   if (event.code === 'Escape') {
+//     return modalClose();
+//   } 
+// };
+
+// Закриття остаточний
+function keyDownModuleWindow(event) {
+    if (event.code === 'Escape') {
+    return modalClose();
+  }  return;
+};
+
+
+function modalOpen(image, alt, index) {
+  modalWindowRef.classList.add('is-open');
+  modalImageRef.src = image;
+  modalImageRef.alt = alt;
+  modalImageRef.dataset.index = index;
+
+  window.addEventListener('keydown', event => keyDownModuleWindow(event));
+};
+
+function modalClose() {
+  modalWindowRef.classList.remove('is-open');
+  modalImageRef.src = '';
+  modalImageRef.alt = '';
+  delete modalImageRef.dataset.index;
+
+  window.removeEventListener('keydown', event => keyDownModuleWindow(event));
+};
